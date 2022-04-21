8148edfc26f5a20050d105ab8eeb55ab057677eb
hsx02 <hsx02self@gmail.com>
2022-04-21 12:50:16 +0530

    mi439: bringup to spark

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mi439.mk
+    $(LOCAL_DIR)/spark_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mi439-user \
-    lineage_mi439-userdebug \
-    lineage_mi439-eng
+    spark_mi439-user \
+    spark_mi439-userdebug \
+    spark_mi439-eng
