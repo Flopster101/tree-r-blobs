50e17aba14bec8868c9ae81610ab981bb6801935
hsx02 <hsx02self@gmail.com>
2022-04-25 14:44:37 +0530

    mi439: bringup to Project Awaken

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2022 The Project Awaken
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mi439.mk
+    $(LOCAL_DIR)/awaken_mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mi439-user \
-    lineage_mi439-userdebug \
-    lineage_mi439-eng
+    awaken_mi439-user \
+    awaken_mi439-userdebug \
+    awaken_mi439-eng
--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2022 The Project Awaken
 #
 # SPDX-License-Identifier: Apache-2.0
 #
